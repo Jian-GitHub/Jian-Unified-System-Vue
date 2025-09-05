c2f3f9ba99b7d8d510743f16b7e342b3f84db9ca
Jian Qi <e.jianqi@gmail.com>
2025-09-05 23:29:52 +0800

    Optimize code structure; Add animation

--- apollo/src/components/login/side/side.vue
+++ apollo/src/components/login/side/side.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 
 // Figma assets
+import CIRCLE_BOTTOM from '@/assets/login/circle_bottom.svg'
 import CIRCLE_TOP from "@/assets/login/circle_top.svg"
-import CIRCLE_BOTTOM  from '@/assets/login/circle_bottom.svg'
-import {ref} from "vue";
+import {ComputedRef, ref} from "vue";
 
 let isLogin = ref(true);
 
@@ -20,20 +20,28 @@ function setIsLogin() {
   }
 }
 
-import { useI18n } from 'vue-i18n'
+import {useI18n} from 'vue-i18n'
 
-const { t, locale } = useI18n()
+const {t, locale} = useI18n()
 
-import { computed } from 'vue'
+import {computed} from 'vue'
+
+const isDark: ComputedRef<boolean> = computed(() => {
+  return document.documentElement.getAttribute('data-theme') === 'dark'
+})
+const title = computed(() => t('side.SIDE_TITLE'))
 const loginSideText1 = computed(() => t('side.login.SIDE_TEXT_1'))
 const loginSideText2 = computed(() => t('side.login.SIDE_TEXT_2'))
-const RegisterSideText1 = computed(() => t('side.registration.SIDE_TEXT_1'))
-const RegisterSideText2 = computed(() => t('side.registration.SIDE_TEXT_2'))
+const registerSideText1 = computed(() => t('side.registration.SIDE_TEXT_1'))
+const registerSideText2 = computed(() => t('side.registration.SIDE_TEXT_2'))
+const loginButtonText = computed(() => t('side.login.TO_REGISTER'))
+const registerButtonText = computed(() => t('side.registration.TO_LOGIN'))
 
 function setTheme(theme: 'light' | 'dark') {
   document.documentElement.setAttribute('data-theme', theme)
   localStorage.setItem('theme', theme) // 可选，持久化保存
 }
+
 // 切换语言
 const switchLanguage = (lang: string) => {
   locale.value = lang
@@ -41,28 +49,26 @@ const switchLanguage = (lang: string) => {
 </script>
 
 <template>
-  <div class="jus-apollo-side" v-if="isLogin">
-    <img class="circle circle--top" :src="CIRCLE_TOP" alt="" />
-    <img class="circle circle--bottom" :src="CIRCLE_BOTTOM" alt="" />
+  <div class="jus-apollo-side">
+    <div class="jus-apollo-side-background">
+      <CIRCLE_BOTTOM :class="['circle', { 'circle--bottom-left' : isLogin }, { 'circle--bottom-right' : !isLogin }]"/>
+      <CIRCLE_TOP :class="['circle', { 'circle--top-right' : isLogin }, { 'circle--top-left' : !isLogin }]"/>
+    </div>
 
-    <div class="title">Jian Unified System</div>
-    <div class="text">{{ loginSideText1 }}<br/>{{ loginSideText2 }}</div>
-
-    <button class="btn" type="button" @click="setIsLogin">
-      前往注册
-    </button>
-  </div>
-
-  <div class="jus-apollo-side" v-else-if="!isLogin">
-    <img class="circle circle--top" :src="CIRCLE_TOP" alt="" />
-    <img class="circle circle--bottom" :src="CIRCLE_BOTTOM" alt="" />
-
-    <div class="title">Jian Unified System</div>
-    <div class="text">{{ RegisterSideText1 }}<br/>{{ RegisterSideText2 }}</div>
-
-    <button class="btn" type="button" @click="setIsLogin">
-      前往登入
-    </button>
+    <div class="jus-apollo-side-content">
+      <div class="title">{{ title }}</div>
+      <div class="text">
+        {{ isLogin ? loginSideText1 : registerSideText1 }}
+        <br/>
+        {{ isLogin ? loginSideText2 : registerSideText2 }}
+      </div>
+      <el-button :class="['btn', { 'btn-zh-spacing': locale === 'zh' }]"
+                 color="var(--jus-color-doraemon-primary-200)"
+                 :dark="isDark"
+                 @click="setIsLogin">
+        {{ isLogin ? loginButtonText : registerButtonText }}
+      </el-button>
+    </div>
   </div>
 
 </template>
@@ -80,26 +86,53 @@ const switchLanguage = (lang: string) => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 12px;
   font-family: "PingFang SC", system-ui, -apple-system, Segoe UI, Roboto, sans-serif;
+  transition: all 1s ease-out; /* 平滑动画 */
+}
+
+.jus-apollo-side-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
 }
 
 .circle {
   position: absolute;
   pointer-events: none;
+  transition: all 1s ease-out; /* 平滑动画 */
 }
-.circle--top {
-  width: 300px;
-  height: 300px;
-  left: calc(50% + 214px);
-  top: calc(50% - 410px);
+/*noinspection CssUnusedSymbol*/
+.circle--top-right {
+  width: 18.75rem;
+  height: 18.75rem;
+  left: calc(50% + 13.375rem);
+  top: calc(50% - 25.625rem);
   transform: translate(-50%, -50%);
 }
-.circle--bottom {
-  width: 500px;
-  height: 500px;
-  left: calc(50% - 192px);
-  top: calc(50% + 452px);
+/*noinspection CssUnusedSymbol*/
+.circle--top-left {
+  width: 18.75rem;
+  height: 18.75rem;
+  left: calc(50% - 13.375rem);
+  top: calc(50% - 25.625rem);
+  transform: translate(-50%, -50%);
+}
+/*noinspection CssUnusedSymbol*/
+.circle--bottom-left {
+  width: 31.25rem;
+  height: 31.25rem;
+  left: calc(50% - 12rem);
+  top: calc(50% + 28.25rem);
+  transform: translate(-50%, -50%);
+}
+/*noinspection CssUnusedSymbol*/
+.circle--bottom-right {
+  width: 31.25rem;
+  height: 31.25rem;
+  left: calc(50% + 12rem);
+  top: calc(50% + 28.25rem);
   transform: translate(-50%, -50%);
 }
 
@@ -122,15 +155,21 @@ const switchLanguage = (lang: string) => {
 }
 
 .btn {
-  width: 180px;
-  height: 50px;
-  border-radius: 8px;
-  background: var(--jus-color-global-neutrals-pure-white);
-  box-shadow: 0 4px 4px rgba(0,0,0,0.25);
+  width: 11.25rem;
+  height: 3.125rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   border: none;
-  font-size: 20px;
-  color: #212121;
-  letter-spacing: 3px;
   cursor: pointer;
+
+  text-align: center;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.5rem; /* 120% */
+}
+/*noinspection CssUnusedSymbol*/
+.btn-zh-spacing {
+  letter-spacing: 0.1875rem;
 }
 </style>
